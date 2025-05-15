f356c5add91572379030c9bfeb53a283b46b8f5f
Jamshidbek070 <xamrakulovjamshid725@gmail.com>
2025-05-15 16:40:44 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-)=e7dtbf95yvg_j$db+896($8^wrcdipdztj6w(f63!#2k0*fn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['examproject-3.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
